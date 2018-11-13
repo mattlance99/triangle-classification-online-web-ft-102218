29c78ab5e3d408c981736a2af57f2fec152d98ad
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-13 23:55:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,10 @@ attr_accessor  :side_one, :side_two, :side_three
   def kind
     if side_one <= 0 || side_two <= 0 || side_three <= 0
        raise TriangleError
-  
+    elsif
+      (side_one + side_two) <= side_three || (side_one + side_three) <= side_two ||  (side_two + side_three) <= side_one 
+      raise TriangleError
+    end
     
     
 
