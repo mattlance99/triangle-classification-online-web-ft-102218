57f021d2a5bf51c3fac655c4a607cff1ae6ad7fb
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-13 22:40:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ attr_accessor  :side_one, :side_two, :side_three
        ((side_two == side_three) && (side_one != side_three))
        :isosceles
      else
-       :
+       :scalene
     end
   end
 
