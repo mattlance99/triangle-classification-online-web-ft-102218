2e40cecdfd87fbb4693dee1c78b9f4c41e1cd8dd
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-13 23:08:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -36,5 +36,8 @@ attr_accessor  :side_one, :side_two, :side_three
     def message 
       "Triangle is Invalid"
     end
+    
+    # if 
+    ((side_one + side_two) < side_three) || ((side_one + side_three) < side_two) ||  ((side_two + side_three) < side_one) then raise TriangleError
   end
 end
